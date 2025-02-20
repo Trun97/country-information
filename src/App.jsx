8950b6625cb2aa4ad30bfd986b7f35b9d022eaff
Trun97 <tarun7283@gmail.com>
2025-02-20 20:26:24 +0100

    opdr 1, stap 1 t/m 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css';
+import axios from 'axios';
+import {useState} from "react";
 
 function App() {
+    const [data, setData] = useState([]);
+
+    async function handleData() {
+        try {
+            const response = await axios.get("https://restcountries.com/v3.1/all?fields=name,flags");
+            console.log(response.data)
+            setData(response.data)
+        } catch (e) {
+            //errors afhandelen (console en UI)
+        } finally {
+
+        }
+    }
 
     return (
         <>
-            Maak je applicatie hier!
+            <button type="button" onClick={handleData}>klik hier</button>
         </>
     )
 }
